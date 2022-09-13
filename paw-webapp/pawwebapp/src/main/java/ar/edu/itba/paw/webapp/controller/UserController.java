58ae2b4ad6a8489a59559cd1885d91781bda8618
Manuel <mdizenhaus@itba.edu.ar>
2022-09-13 11:11:42 -0300

    adding sellerprofile in controller

--- paw-webapp/pawwebapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
+++ paw-webapp/pawwebapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.interfaces.services.SellerService;
 import ar.edu.itba.paw.interfaces.services.UserService;
+import ar.edu.itba.paw.models.Seller;
 import ar.edu.itba.paw.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,6 +19,9 @@ public class UserController {
     private UserService userService;
 
     @Autowired
+    private SellerService sellerService;
+
+    @Autowired
     private AuthenticationController authController;
 
 
@@ -29,6 +34,22 @@ public class UserController {
         final ModelAndView mav = new ModelAndView("userProfile");
         Optional<User> user = userService.findByEmail(authController.getLoggedEmail());
         if(!user.isPresent()) throw new IllegalStateException("no lo ovaf dkfds");
+        mav.addObject("user", user.get());
+        return mav;
+    }
+
+    @RequestMapping(value="/sellerProfile")
+    public ModelAndView sellerProfile(){
+        final ModelAndView mav = new ModelAndView("sellerProfile");
+        Optional<User> user = userService.findByEmail(authController.getLoggedEmail());
+        if(!user.isPresent()) throw new IllegalStateException("No se encntró user");
+        Optional<Seller> seller = sellerService.findById(user.get().getId());
+        if(!seller.isPresent()) throw new IllegalStateException("No se encontró seller");
+
+        // TODO: Acá faltaría además buscar los productos que vende un seller,
+        //  y las órdenes que tiene pendientes (para esto hay que agrandar la BDD)
+
+        mav.addObject("seller", seller.get());
         mav.addObject("user", user.get());
         return mav;
     }
