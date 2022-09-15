300b4a7b268d6bf5f31643e1baf52520355eaf15
Manuel <mdizenhaus@itba.edu.ar>
2022-09-14 23:07:57 -0300

    pass faqcontroller logic to basic controller

--- paw-webapp/pawwebapp/src/main/java/ar/edu/itba/paw/webapp/controller/BasicController.java
+++ paw-webapp/pawwebapp/src/main/java/ar/edu/itba/paw/webapp/controller/BasicController.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.interfaces.services.FAQService;
 import ar.edu.itba.paw.interfaces.services.UserService;
 import ar.edu.itba.paw.webapp.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +10,17 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class BasicController {
 
     private final UserService us;
+    private final FAQService faqService;
 
     @Autowired
-    public BasicController(final UserService us){
+    public BasicController(final UserService us, FAQService faqService){
         this.us = us;
+        this.faqService = faqService;
     }
 
     //    @RequestMapping(value = "/", method = RequestMethod.GET, headers = ..., consumes = ..., produces = ...)
@@ -28,28 +30,18 @@ public class BasicController {
         return mav;
     }
 
-    @RequestMapping("/FAQ")
-    public ModelAndView faq(){
-        final ModelAndView mav = new ModelAndView("FAQ");
-        return mav;
-    }
-
     @RequestMapping("/login")
     public ModelAndView login(HttpServletRequest request){
         final ModelAndView mav = new ModelAndView("login");
         return mav;
     }
 
-    //Puedo agregar una validacion como expresion regular para userId
-    @RequestMapping("/profile/{userId:[0-9]+}")
-    public ModelAndView profile(@PathVariable("userId") final long userId) {
-        final ModelAndView mav = new ModelAndView("profile");
-        mav.addObject("USER", us.findById(userId).orElseThrow(UserNotFoundException::new));
+    @RequestMapping(value="/faq")
+    public ModelAndView faq(){
+        final ModelAndView mav = new ModelAndView("FAQ");
+        mav.addObject("faqs", faqService.getFAQs());
         return mav;
-        // Con esto puedo ingresar a:
-        // http://localhost:8080/profile/1
     }
-
 
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
