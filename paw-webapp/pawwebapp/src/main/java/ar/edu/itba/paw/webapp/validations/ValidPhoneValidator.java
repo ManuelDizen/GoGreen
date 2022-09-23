78a52e970e2547caad904cfeebe3407caae869a0
Manuel <mdizenhaus@itba.edu.ar>
2022-09-22 22:28:17 -0300

    tweak

--- paw-webapp/pawwebapp/src/main/java/ar/edu/itba/paw/webapp/validations/ValidPhoneValidator.java
+++ paw-webapp/pawwebapp/src/main/java/ar/edu/itba/paw/webapp/validations/ValidPhoneValidator.java
@@ -10,10 +10,16 @@ public class ValidPhoneValidator implements ConstraintValidator<ValidPhone, Stri
     final Pattern pattern = Pattern.compile(
             "^\\s?((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?\\s?");
      */
+
+    // Esta tiene codigo de area opcional, y 4 bloques posibles (minimo 2) de entre 1 y 4 numeros
+    // lo que permtie que +54 1 1 1 1 sea un numero valido
     final Pattern pattern = Pattern.compile(
             "^\\s?(\\+[0-9]{1,3}[ \\-]*)?" +
                     "([0-9]{1,4}[ \\-]*){2,4}\\s?"
     );
+
+    // La diferencia aca es que puede aparecer algo como +54 1 1 pero los siguientes dos
+    // bloques de numeros deben tener entre 3 y 4 caracteres (y son obligatorios).
 
     final Pattern patternSuggestion = Pattern.compile(
             "^\\s?(\\+[0-9]{1,3}[ \\-]*)?" +
