5106af31d7a95cd53bbaabfb33d4ec6c192903d6
Manuel <mdizenhaus@itba.edu.ar>
2022-10-25 10:02:34 -0300

    article exception handling i

--- paw-webapp/services/src/main/java/ar/edu/itba/paw/services/ArticleServiceImpl.java
+++ paw-webapp/services/src/main/java/ar/edu/itba/paw/services/ArticleServiceImpl.java
@@ -3,10 +3,13 @@ package ar.edu.itba.paw.services;
 import ar.edu.itba.paw.interfaces.persistence.ArticleDao;
 import ar.edu.itba.paw.interfaces.services.ArticleService;
 import ar.edu.itba.paw.interfaces.services.ImageService;
-import ar.edu.itba.paw.models.Article;
-import ar.edu.itba.paw.models.Image;
-import ar.edu.itba.paw.models.Seller;
+import ar.edu.itba.paw.interfaces.services.SecurityService;
+import ar.edu.itba.paw.interfaces.services.SellerService;
+import ar.edu.itba.paw.models.*;
 import ar.edu.itba.paw.models.exceptions.ArticleNotFoundException;
+import ar.edu.itba.paw.models.exceptions.ForbiddenActionException;
+import ar.edu.itba.paw.models.exceptions.UnauthorizedRoleException;
+import ar.edu.itba.paw.models.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,11 +22,16 @@ public class ArticleServiceImpl implements ArticleService {
 
     private final ImageService imageService;
     private final ArticleDao articleDao;
+    private final SecurityService securityService;
+    private final SellerService sellerService;
 
     @Autowired
-    public ArticleServiceImpl(final ImageService imageService, final ArticleDao articleDao) {
+    public ArticleServiceImpl(final ImageService imageService, final ArticleDao articleDao,
+                              final SecurityService securityService, final SellerService sellerService) {
         this.imageService = imageService;
         this.articleDao = articleDao;
+        this.securityService = securityService;
+        this.sellerService = sellerService;
     }
 
     private Image parseByteArrayToImage(byte[] image){
@@ -50,9 +58,24 @@ public class ArticleServiceImpl implements ArticleService {
         return articleDao.getById(id);
     }
 
+    public Boolean checkForArticleOwnership(long id) {
+        User user = securityService.getLoggedUser();
+        if(user == null) throw new UnauthorizedRoleException();
+        Optional<Seller> maybeSeller = sellerService.findByUserId(user.getId());
+        if(!maybeSeller.isPresent()) throw new UserNotFoundException();
+        Seller seller = maybeSeller.get();
+        Optional<Article> maybeArticle = getById(id);
+        if(maybeArticle.isPresent()){
+            Article article = maybeArticle.get();
+            return article.getSeller().getId().equals(seller.getId());
+        }
+        return false;
+    }
     @Transactional
     @Override
     public void edit(Long id, String newMessage, byte[] newImage) {
+        if(!checkForArticleOwnership(id)){throw new ForbiddenActionException();}
+
         Optional<Article> maybeArticle = articleDao.getById(id);
         if(!maybeArticle.isPresent()) throw new ArticleNotFoundException();
         Article article = maybeArticle.get();
